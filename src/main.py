3667531aa71484da5e54d389d9fbc44e9ae21d7e
Rasmus Tanggaard <rasmus@tthost.dk>
2023-08-08 12:25:55 +0200

    General file header

--- src/main.py
+++ src/main.py
@@ -1,3 +1,13 @@
+# Project Name: 
+#   TBD
+#
+# Project Description: 
+#   Program meant to automate the process of downloading 
+#   videos from twitch after a stream has ended and uploading them to youtube.
+# 
+# Authors: 
+#   Rasmus Tanggaard, Malthe Sørensen, Jonas Søgaard Frederiksen
+
 
 
 def main():
