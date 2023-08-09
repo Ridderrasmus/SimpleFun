aea35cdd4d24082924aa6353b91acd1852815ca9
Rasmus Tanggaard <rasmus@tthost.dk>
2023-08-09 12:39:31 +0200

    Added some main file plans

--- src/main.py
+++ src/main.py
@@ -11,10 +11,38 @@
 # Authors: 
 #   Rasmus Tanggaard, Malthe Sørensen, Jonas Søgaard Frederiksen
 
+import asyncio
+from Stream.StreamerManager import StreamerManager
+from Youtube.YoutubeManager import YoutubeManager
+
+
+looping = True
 
 
 def main():
-    print("Hello World")
+    streamerManager = StreamerManager()
+    youtubeManager = YoutubeManager()
+    
+    twitch_account = input("Please enter the username of your twitch account: ")
+    
+    streamerManager.addStreamer(twitch_account)
+    
+    asyncio.run(task_loop(streamerManager, youtubeManager))
+    
+    
+async def task_loop(strmManager, ytManager):
+    while looping:
+        # Check for streamers with new vods
+        pass
+        
+        
+async def handle_vod(strmManager):
+    # Handle what happens when a streamer goes offline and a vod is created
+    ## Download vod into temporary folder
+    ## Upload vod to youtube with the title matching the vod title
+    pass
+        
+        
 
 
 if __name__ == "__main__":
